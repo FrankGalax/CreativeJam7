8d769a2cc09ac09e37babf9ffa3bd8211e9f8b79
Francis <francis-r-emond@hotmail.com>
2017-01-06 21:08:00 -0500

    main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    private string m_Username;
+    private Button m_PlayButton;
+
+    void Start()
+    {
+        m_PlayButton = Transform.FindObjectOfType<Canvas>().transform.Find("PlayButton").GetComponent<Button>();
+        m_PlayButton.enabled = false;
+    }
+
+    public void SetUsername(string username)
+    {
+        m_Username = username;
+        m_PlayButton.enabled = username.Length > 0;
+    }
+
+    public void Play()
+    {
+        Debug.Log(m_Username);
+    }
+}
