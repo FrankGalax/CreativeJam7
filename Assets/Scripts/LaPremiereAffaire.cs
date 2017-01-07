f3e5bd4c2dbcbf5e2ae64395de86b362c1331056
Francis <francis-r-emond@hotmail.com>
2017-01-06 22:25:40 -0500

    basic network

---
+++ Assets/Scripts/LaPremiereAffaire.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LaPremiereAffaire : MonoBehaviour
+{
+	void Start ()
+    {
+        INetwork network = INetwork.Instance;
+        if (!network.IsConnected)
+        {
+            SceneManager.LoadScene("mainMenu");
+            return;
+        }
+	}
+}
