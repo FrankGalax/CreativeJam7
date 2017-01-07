193cbfcc994152ba31ffd6da9e6256f6fb98b9cf
unknown <nadjam@337-B6.nad.qc.ca>
2017-01-07 01:15:44 -0500

    Money everywhere

--- Assets/Scripts/Environment/PlanetFragment.cs
+++ Assets/Scripts/Environment/PlanetFragment.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class PlanetFragment : MonoBehaviour {
 
+    public int TotalPointValue = 0;
+    public float GrosCashDropRate = 0.0f;
+
     // Use this for initialization
     void Start () {
-
+        TakeDamage();
     }
 	
 	// Update is called once per frame
@@ -21,11 +24,46 @@ public class PlanetFragment : MonoBehaviour {
 
     private void Destroy()
     {
-        Instantiate(ResourceManager.GetPrefab("Volcano"), transform.position, transform.rotation);
+        GameObject layer1 =  transform.Find("Layer1Graphics/Layer1Fragment").gameObject;
+        if (layer1.GetComponent<Renderer>().enabled)
+        {
+            layer1.GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            GameObject layer2 = transform.Find("Layer2Graphics/Layer2Fragment").gameObject;
+            if (layer2.GetComponent<Renderer>().enabled)
+            {
+                layer2.GetComponent<Renderer>().enabled = false;
+                Instantiate(ResourceManager.GetPrefab("Volcano"), layer2.transform.position, layer2.transform.rotation);
+            }
+        }
     }
 
     private void GenerateResources()
     {
+        int grosCashValue = ResourceManager.GetPrefab("GrosCashSale").GetComponent<GrosCashSale>().Value;
+        int petiteMonnaieValue = ResourceManager.GetPrefab("PetiteMonnaie").GetComponent<PetitCashSale>().Value;
 
+        int cumulativePointValueSpawned = 0;
+        while (cumulativePointValueSpawned < TotalPointValue)
+        {
+            Vector3 direction = Quaternion.AngleAxis(Random.Range(-45, 45), Vector3.right) * transform.up;
+            direction = Quaternion.AngleAxis(Random.Range(-45, 45), Vector3.forward) * direction;
+            direction.Normalize();
+
+            if (cumulativePointValueSpawned + grosCashValue <= TotalPointValue && Random.Range(0.0f, 1.0f) < GrosCashDropRate)
+            {
+                GameObject grosCashSale = (GameObject)Instantiate(ResourceManager.GetPrefab("GrosCashSale"), transform.position, transform.rotation);
+                grosCashSale.GetComponent<Rigidbody>().velocity = direction;
+                cumulativePointValueSpawned += grosCashValue;
+            }
+            else
+            {
+                GameObject petiteMonnaie = (GameObject)Instantiate(ResourceManager.GetPrefab("PetiteMonnaie"), transform.position, transform.rotation);
+                petiteMonnaie.GetComponent<Rigidbody>().velocity = direction;
+                cumulativePointValueSpawned += petiteMonnaieValue;
+            }
+        }
     }
 }
